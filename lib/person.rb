f833935b58e829522323c1a626e2cfad2e393433
jbshipman <jbshipman@icloud.com>
2020-01-28 14:51:06 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  # initialize required args
+  def initialize(name)
+    @name = name
+  end
+  
+  # modify @name functionality
+  def name=(name)
+    @name = name
+  end
+
+  # get @name value functionality
+  def name
+    @name
+  end
+end
